5c44d200cd342cc326d0b1b249efd4cd5d836d38
Machyne <mcotter222@gmail.com>
2013-10-22 00:07:38 -0500

    Add mongoose to handle data base things, and build a demo for users

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/carlhacks');
+var db = mongoose.connection;
 var app = express();
 
 // all environments
@@ -27,7 +30,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log('Mongo db ready!');
+});
+var User = mongoose.model('User', {name: String, email: String})
+
 app.get('/', routes.index);
+
+app.get('/users', function (req, res){
+  User.find({}, function (error, users){
+    if (error) console.log("error!");
+    res.render('usersList.jade', {
+      title: 'Users',
+      users: users,
+    });
+  });
+});
+
+app.post('/users/new', function (req, res) {
+  var user = new User({
+      name: req.param('name'),
+      email: req.param('email')
+  });
+  user.save(function (error, user) {
+    if(error) console.log("error!");
+  });
+  res.redirect('/');
+})
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
