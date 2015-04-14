457ee67da5a86737e02d8a84ce4218d5ccc2266b
Matt Cotter <mcotter222@gmail.com>
2015-04-13 23:18:25 -0500

    add support for multiple emails

--- mailer.py
+++ mailer.py
@@ -2,11 +2,35 @@ import urllib
 
 from envelopes import Envelope, GMailSMTP
 
-from to_accept import accept
 from config import PASS
+from to_accept import accept
+from to_exclude import exclude
 
+
+ACCEPT = (u"Welcome to CarlHacks!\n\n"
+           "This email confirms that you are accepted to CarlHacks. "
+           "We are looking forward to seeing you at the event!\n\n"
+           "Please fill out this form to confirm your attendance:\n"
+           "{form_url}\n\nAlso check out our \"What to Bring\" list:\n"
+           "http://carlhacks.io/bring\n\n"
+           "Unfortunately, travel grant decisions are not available "
+           "at this time.\n\n"
+           "Feel free to reply to this email with any questions.\n"
+           "See you soon!\nThe CarlHacks Team",
+          u'CarlHacks Acceptance')
+
+REMIND = (u"Hi {name},\n\n"
+           "This is just a reminder to fill out this form to confirm or "
+           "withdraw your CarlHacks attendance:\n{form_url}\n\n"
+           "If you have any questions, feel free to respond to this email.\n\n"
+           "We hope to see you at CarlHacks!\nThe CarlHacks Team",
+          u'Confirm Your CarlHacks Attendance')
+
+USE = REMIND
 
 for name, email, id_ in accept:
+    if email in exclude:
+        continue
     name_esc = urllib.quote_plus(name)
     print 'emailing {} <{}>, id: {}'.format(name, email, id_)
     url = ("https://docs.google.com/forms/d/1slf5fwjCimmfcDTcwpFWYbcYOtLkMiPk9"
@@ -15,17 +39,12 @@ for name, email, id_ in accept:
     envelope = Envelope(
         from_addr=(u'info@carlhacks.io', u'CarlHacks'),
         to_addr=(email, name),
-        subject=u'CarlHacks Acceptance',
-        text_body=(u"Welcome to CarlHacks!\n\n"
-                    "This email confirms that you are accepted to CarlHacks. "
-                    "We are looking forward to seeing you at the event!\n\n"
-                    "Please fill out this form to confirm your attendance:\n"
-                    "{}\n\nAlso check out our \"What to Bring\" list:\n"
-                    "http://carlhacks.io/bring\n\n"
-                    "Unfortunately, travel grant decisions are not available "
-                    "at this time.\n\n"
-                    "Feel free to reply to this email with any questions.\n"
-                    "See you soon!\nThe CarlHacks Team".format(url))
+        subject=USE[1],
+        text_body=USE[0].format(**{
+            'form_url': url,
+            'name': name.rpartition(' ')[0],
+            'email': email,
+        })
     )
     try:
         envelope.send('mail.gandi.net', login='info@carlhacks.io',
