b4834d815ea7e21dcd6be5ccc5ca633aae6df7ea
Matt Cotter <mcotter222@gmail.com>
2015-05-02 15:22:27 -0500

    more robust

--- resumes.py
+++ resumes.py
@@ -7,6 +7,8 @@ def main():
     db = client.carlhacks
     for email in db.users.distinct('email'):
         usr = db.users.find({'email': email})[0]
+        if 'resume' not in usr or not usr['resume']:
+            continue
         respath = usr['resume']['path']
         ext = ''.join(respath.rpartition('.')[1:])
         shutil.copy2(respath, 'res/{}{}'.format(email, ext))
