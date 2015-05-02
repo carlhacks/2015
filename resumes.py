9b1f13295a703a30246d7ffd3ea1b44713cef6aa
Matt Cotter <mcotter222@gmail.com>
2015-05-02 15:20:13 -0500

    add resume fixer

---
+++ resumes.py
@@ -0,0 +1,16 @@
+import shutil
+
+import pymongo
+
+def main():
+    client = pymongo.MongoClient()
+    db = client.carlhacks
+    for email in db.users.distinct('email'):
+        usr = db.users.find({'email': email})[0]
+        respath = usr['resume']['path']
+        ext = ''.join(respath.rpartition('.')[1:])
+        shutil.copy2(respath, 'res/{}{}'.format(email, ext))
+
+
+if __name__ == '__main__':
+    main()
